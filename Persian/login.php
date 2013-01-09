72fbad9709f063253c3286938d07a4872970b6ec
masoodking <eng.masood88@gmail.com>
2013-01-09 22:25:30 +0330

    Update Persian/login.php

--- Persian/login.php
+++ Persian/login.php
@@ -4,23 +4,23 @@
 $lang_login = array(
 
 // Miscellaneous
-'Wrong user/pass'			=>	'نام کاربری یا رمز عبور معتبر نیست.',
+'Wrong user/pass'  		=>	'نام کاربری یا رمز عبور معتبر نیست.',
 'Forgotten pass'			=>	'رمز خود را فراموش کرده اید؟',
 'Login redirect'			=>	'ورود با موفقیت انجام شد. در حال انتقال...',
 'Logout redirect'			=>	'خروج انجام شد. در حال انتقال',
 'No email match'			=>	'کاربری با ایمیل داده شده ثبت نام نکرده است',
 'Request pass'				=>	'درخواست رمز عبور',
 'Request pass legend'		=>	'آدرس ایمیلی که با آن ثبت نام کرده اید را وارد کنید',
-'Request pass info'			=>	'A new password together with a link to activate the new password will be sent to that address.',
+'Request pass info'			=>	'یک پسوورد جدید به همراه یک لین برای فعال سازی آن به آدرس ایمیل ارسال شد.',
 'Not registered'			=>	'هنوز ثبت نام نکرده اید؟',
 'Login legend'				=>	'نام کاربری و رمز عبور خود را در زیر وارد کنید',
 'Remember me'				=>	'مرا به یاد داشته باش.',
-'Login info'				=>	'If you have not registered or have forgotten your password click on the appropriate link below.',
+'Login info'				=>	'اگر ثبت نام نکرده اید با روز عبور خود را فراموش کرده اید روی لینک مربوط کلیک کنید',
 'New password errors'		=>	'خطا در درخواست رمز عبور',
-'New passworderrors info'	=>	'The following error needs to be corrected before a new password can be sent:',
+'New passworderrors info'	=>	'خطا های زیر را درست کنید تا پسوورد برای شما ارسال شود:',
 
 // Forget password mail stuff
-'Forget mail'				=>	'An email has been sent to the specified address with instructions on how to change your password. If it does not arrive you can contact the forum administrator at',
-'Email flood'				=>	'This account has already requested a password reset in the past hour. Please wait a while before requesting a new password again.'
+'Forget mail'				=>	'نحوه ی تعویض گذرواژه ی شما به آدرس مشخص شده ارسال شد. اگر ایمیل را دریافت نکردید به مدیریت اطلاع دهید',
+'Email flood'				=>	'برای این حساب کاربری در یک ساعت اخیر درخواست گذرواژه شده است. برای ثبت درخواست جدید باید چند دقیقه دیگر اقدام کنید.'
 
 );
